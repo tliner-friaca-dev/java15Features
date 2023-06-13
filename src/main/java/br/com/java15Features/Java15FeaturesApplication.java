44f19b3b0bb2a3f110a9e5febc4829e34c222d4e
tliner-friaca-dev <tlinerfc@hotmail.com>
2023-06-13 15:30:20 -0300

    first commit

---
+++ src/main/java/br/com/java15Features/Java15FeaturesApplication.java
@@ -0,0 +1,18 @@
+package br.com.java15Features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java15FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java15FeaturesApplication.class, args);
+
+		System.out.println("\n----------------------");
+		System.out.println("ENTROU NA APLICAÇÃO!!!");
+		System.out.println("----------------------\n");
+		
+	}
+
+}
